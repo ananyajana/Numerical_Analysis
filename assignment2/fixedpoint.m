525de9aaeaf57a911a5760048e3e0923c905e22b
Ananya Jana <ananya.jana@gmail.com>
2018-03-15 19:35:32 +0530

    fixed
    Signed-off-by: Ananya Jana <ananya.jana@gmail.com>

--- assignment2/fixedpoint.m
+++ assignment2/fixedpoint.m
@@ -1,20 +1,22 @@
 
 x0 = input('Please enter a value for x: ');
-index = input('Please enter the index value: ');
+%index = input('Please enter the index value: ');
 max_iterate = input('Please enter the maximum number of iteration: ');
 error_bd = input('Please enter the error bound for the function: ');
 
-[root] = fixed_point_iteration(x0, error_bd, max_iterate, index);
+[root] = fixed_point_iteration(x0, error_bd, max_iterate);
 
-function [root] = fixed_point_iteration(x0,error_bd,max_iterate, index)
+function [root] = fixed_point_iteration(x0, error_bd, max_iterate)
 syms x;
 syms z;
-f = x^2 - 3*x +2;
+f = x^2 - 3*x + 2;
 g= 2*x - 3;
 % g=(x^2 - 2)*(2*x - 3);
 % g = 3 - 2/x;
 %g = sqrt(3*x - 2);
 % g = (x^2 + 2)/3;
+
+it_count = 0;
 
 x1 = x0;
 x2 = subs(g, 'x', x1);
@@ -23,5 +25,7 @@ while abs(x2-x1) > error_bd && it_count <= max_iterate
     x1 = x2;
     x2 = subs(g, 'x', x1);
     disp([x1 x2 abs(x2-x1)]);
+    it_count = it_count + 1;
 end
+root = x2;
 end
